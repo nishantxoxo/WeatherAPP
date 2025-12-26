2da887424c65ecceb9ba3ccaaa98c5e7fe33d2ea
nishant <nishantpriya.scorpion@gmail.com>
2025-12-27 00:06:41 +0530

    building api

---
+++ app/src/main/java/com/plcoding/weatherapp/data/remote/WeatherApi.kt
@@ -0,0 +1,11 @@
+package com.plcoding.weatherapp.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+    @GET("v1/forecast?hourly=temperature_2m,relative_humidity_2m,weathercode,windspeed_10m,pressure_msl")
+    suspend fun getWeatherData( @Query("latitute") lat: Double,
+                                @Query("longitute") long: Double,
+                                )
+}
