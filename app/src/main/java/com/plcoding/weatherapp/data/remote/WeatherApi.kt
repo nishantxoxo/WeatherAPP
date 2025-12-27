504daa7086a5312ddd5305a15d9b4bbc9f2e1142
nishant <nishantpriya.scorpion@gmail.com>
2025-12-27 15:06:10 +0530

    weatherData

--- app/src/main/java/com/plcoding/weatherapp/data/remote/WeatherApi.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/remote/WeatherApi.kt
@@ -7,5 +7,5 @@ interface WeatherApi {
     @GET("v1/forecast?hourly=temperature_2m,relative_humidity_2m,weathercode,windspeed_10m,pressure_msl")
     suspend fun getWeatherData( @Query("latitute") lat: Double,
                                 @Query("longitute") long: Double,
-                                )
+                                ) : WeatherDto
 }
